e30f25b312c51b6a3e80961a534c9eae80d0efe4
KrisD123 <dodonova.kristina@gmail.com>
2015-12-14 16:33:45 +0300

    page object structure

--- src/test/java/ru/st/selenium/pages/TestBase.java
+++ src/test/java/ru/st/selenium/pages/TestBase.java
@@ -1,53 +1,26 @@
 package ru.st.selenium.pages;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 
-import ru.stqa.selenium.factory.WebDriverFactory;
+import ru.st.selenium.applogic.ApplicationManager;
+import ru.st.selenium.applogic0.ApplicationManager0;
+import ru.st.selenium.applogic2.ApplicationManager2;
+import ru.st.selenium.model.User;
 
-import ru.st.selenium.util.PropertyLoader;
-
-/**
- * Base class for all the TestNG-based test classes
- */
 public class TestBase {
-	protected WebDriver driver;
 
-	protected String gridHubUrl;
-
-	protected String baseUrl;
+	public static User ADMIN = new User().setLogin("admin").setPassword("admin");
+	protected ApplicationManager app;
 
 	@BeforeClass
 	public void init() {
-		baseUrl = PropertyLoader.loadProperty("site.url");
-		gridHubUrl = PropertyLoader.loadProperty("grid2.hub");
-
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setBrowserName(PropertyLoader.loadProperty("browser.name"));
-		capabilities.setVersion(PropertyLoader.loadProperty("browser.version"));
-		String platform = PropertyLoader.loadProperty("browser.platform");
-		if (!(null == platform || "".equals(platform))) {
-			capabilities.setPlatform(Platform.valueOf(PropertyLoader.loadProperty("browser.platform")));
-		}
-
-		if (!(null == gridHubUrl || "".equals(gridHubUrl))) {
-			driver = WebDriverFactory.getDriver(gridHubUrl, capabilities);
-		} else {
-			driver = WebDriverFactory.getDriver(capabilities);
-		}
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		app = new ApplicationManager2();
+	}
+	
+	@AfterSuite
+	public void stop() {
+	  app.stop();
 	}
 
-	@AfterSuite(alwaysRun = true)
-	public void tearDown() {
-		if (driver != null) {
-			WebDriverFactory.dismissDriver(driver);
-		}
-	}
 }
